1cd913731015e7caf856c1f37ffbeadae702394d
Eismer Lobaina <eismerlobaina@gmail.com>
2023-10-03 22:26:46 -0400

    feat/bgAndGradients and flexbox

--- src/AnimacionPage/animacion.js
+++ src/AnimacionPage/animacion.js
@@ -1,12 +1,25 @@
-const themeToggle = document.getElementById("theme-toggle");
+const themeToggleLight = document.getElementById("theme-toggle-light");
+const themeToggleDark = document.getElementById("theme-toggle-dark");
+const themesContainer = document.getElementById("themes-container");
 const body = document.body;
 
-themeToggle.addEventListener("click", () => {
+themeToggleDark.addEventListener("click", () => {
+  themesContainer.classList.remove("light");
+  themesContainer.classList.add("dark");
+
+  body.classList.toggle("dark");
+});
+
+themeToggleLight.addEventListener("click", () => {
+  themesContainer.classList.remove("dark");
+  themesContainer.classList.add("light");
+
   body.classList.toggle("dark");
 });
 
 // Almacenar la selección de tema
 themeToggle.addEventListener("click", () => {
+  console.log("click");
   if (body.classList.contains("light")) {
     body.classList.remove("light");
     body.classList.add("dark");
